ddbd7a750bec626f788586f7e59b775f163bf38a
ksagar03 <sagar22krishna03@gmail.com>
2024-11-14 21:05:31 +0530

    Update: Working on add newExpense page

--- app/Home/newExp/page.tsx
+++ app/Home/newExp/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { fetchCategories, addCategory } from "@/app/lib/axios";
+import { addCategory, fetchCategories } from "@/app/lib/axios";
 
 const page = () => {
   const [categories, setCategories] = useState<string[]>([]);
@@ -10,6 +10,8 @@ const page = () => {
   useEffect(() => {
     const loadCategories = async () => {
       try {
+        const fetchedCategory = await fetchCategories()
+        setCategories(fetchedCategory)
       } catch (error) {
         setError(`failed to load categories ${error}`);
       }
@@ -29,7 +31,19 @@ const page = () => {
       setError("Failed to add category");
     }
   };
-  return <div>New Expenses</div>;
+  return <div className=" flex justify-center">
+    <h2 className="text-lg font-semibold"> Search for the Category:</h2>
+    <ul>
+      {categories.map((category , index) => (
+        <li key={index}>{category}</li>
+      ))}
+    </ul>
+    <input type="text" value={newCategory} onChange={(e) => setNewCategory(e.target.value)}
+    placeholder="Search Category" />
+    <button onClick={handleAddCategory}>Add Category</button>
+    
+
+  </div>;
 };
 
 export default page;
---
+++ app/api/categories/route.ts
@@ -0,0 +1,206 @@
+// // import type { NextApiRequest, NextApiResponse } from "next";
+// // import openDb from "@/app/lib/db";
+
+// // type Category = {
+// //   id: number;
+// //   name: string;
+// // };
+// // type Data = Category | Category[] | { error: string };
+
+// // const sendErrorResponse = (
+// //   res: NextApiResponse<Data>,
+// //   statusCode: number,
+// //   message: string
+// // ) => {
+// //   res.status(statusCode).json({ error: message });
+// // };
+
+// // const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+// //   const db = await openDb();
+
+// //   if (req.method !== "POST" && req.method !== "GET") {
+// //     return sendErrorResponse(res, 405, "Method not allowed");
+// //   }
+
+// //   if (req.method == "POST") {
+// //     const { name } = req.body;
+// //     if (!name) {
+// //       return sendErrorResponse(res, 400, "Name is required");
+// //     }
+
+// //     try {
+// //       const result = await db.run("INSERT INTO users (name) VALUES (?)", name);
+// //       if (typeof result.lastID !== "number") {
+// //         return sendErrorResponse(res, 500, "Failed to get last inserted ID");
+// //       }
+// //       return res.status(201).json({ id: result.lastID, name });
+// //     } catch (error) {
+// //       return sendErrorResponse(res, 400, "Failed to add name.");
+// //     }
+// //   }
+// //   // else{
+// //   //   res.status(405).json({error: "method not allowed"})
+// //   // }
+// //   if (req.method == "GET") {
+// //     const { name } = req.query;
+
+// //     try {
+// //       if (name) {
+// //         const query = `%${name}%`;
+// //         const categories: Category[] = await db.all(
+// //           "SELECT id, name FROM categories WHERE name LIKE ? LIMIT 5 ",
+// //           query
+// //         );
+// //         return res.status(200).json(categories);
+// //       }
+// //       // if there is no query then
+// //       const categories: Category[] = await db.all(
+// //         "SELECT id, name FROM categories LIMIT 5"
+// //       );
+// //       return res.status(200).json(categories);
+// //     } catch (error) {
+// //       console.error(error);
+// //       return sendErrorResponse(res, 500, "Internal Server Error");
+// //     }
+// //   }
+// // };
+
+// // export default handler;
+// // Above code is for sqlite which is not feasable with Vercel
+
+// "use server";
+// import dbConnect from "@/app/lib/dbConnect";
+// import Categories from "@/app/models/categoriesModel";
+// import { NextApiRequest, NextApiResponse } from "next";
+
+// let cachedCategories: string[] | null | undefined = undefined;
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   await dbConnect();
+
+//   const { method, query } = req;
+//   const searchQuery = query.search as string;
+
+//   switch (req.method) {
+//     case "GET":
+//       try {
+//         if (cachedCategories === null || cachedCategories === undefined) {
+//           const categoryDoc = await Categories.findOne();
+//           cachedCategories = categoryDoc ? categoryDoc.categories : [];
+//         }
+
+//         let filterCategories = cachedCategories;
+
+//         if (searchQuery) {
+//           filterCategories = cachedCategories?.filter((category) =>
+//             category.toLowerCase().includes(searchQuery.toLowerCase())
+//           );
+//         }
+//         res.status(200).json({ success: true, categories: filterCategories });
+//       } catch (error) {
+//         res
+//           .status(500)
+//           .json({ success: false, error: "Failed to fetch categories" });
+//       }
+//       break;
+//     case "POST":
+//       try {
+//         const { newCategory } = req.body;
+
+//         if (!newCategory) {
+//           return res
+//             .status(400)
+//             .json({ success: false, error: "Category name is required" });
+//         }
+
+//         let categoryData = await Categories.findOne();
+//         if (!categoryData) {
+//           categoryData = new Categories({ categories: [] });
+//         }
+
+//         if (!categoryData.categories.includes(newCategory)) {
+//           categoryData.categories.push(newCategory);
+//           await categoryData.save();
+//           cachedCategories = categoryData.categories;
+//         }
+//         res.status(201).json({ success: true, category: newCategory });
+//       } catch (error) {
+//         res
+//           .status(500)
+//           .json({ success: false, error: "Failed to add category" });
+//       }
+//       break;
+
+//     default:
+//       res.setHeader("Allow", ["Get", "POST"]);
+//       res.status(405).end(`Method ${method} is not allowed`);
+//   }
+// }
+//  this above Post and Get method will also not work, since the next js 13's App Router, requires named exports for each HTTP method (GET, POST, etc.),
+
+import { NextResponse, NextRequest } from "next/server";
+import dbConnect from "@/app/lib/dbConnect";
+import Categories from "@/app/models/categoriesModel";
+import { Category } from "@mui/icons-material";
+
+let cachedCategories: string[] | null | undefined = undefined;
+
+// GET
+
+export async function GET(req: NextRequest) {
+  await dbConnect();
+
+  const searchQuery = req.nextUrl.searchParams.get("search") || "";
+
+  try {
+    if (cachedCategories === null || cachedCategories === undefined) {
+      const categoryData = await Categories.findOne();
+      cachedCategories = categoryData ? categoryData.categories : [];
+    }
+
+    let filterCategories: string[] = cachedCategories || [];
+    if (searchQuery) {
+      filterCategories = filterCategories.filter((category) =>
+        category.toLowerCase().includes(searchQuery.toLocaleLowerCase())
+      );
+    }
+    return NextResponse.json({success: true, categories: filterCategories},{status: 200})
+  } catch(error) {
+    return NextResponse.json({succes: false , error: "Failed to fetch categories"}, {status: 500})
+  }
+}
+
+
+//  POST 
+
+export async function POST(req: NextRequest){
+  await dbConnect();
+  try{
+    const {newCategory} = await req.json();
+
+    if(!newCategory){
+      return NextResponse.json({success: false, error: 'Category name is required' }, { status: 400 })
+    }
+    let categoryData = await Categories.findOne()
+    
+    if(!categoryData){
+      categoryData = new Categories({ categories: []})
+
+    }
+
+    if(!categoryData.categories.includes(newCategory)){
+      categoryData.categories.push(newCategory)
+      await categoryData.save()
+      cachedCategories = categoryData.categories
+    }
+
+    return NextResponse.json({success: true, category: newCategory}, {status: 201})
+
+  }catch(error){
+    return NextResponse.json({success: false, error: "Failed to add category" }, {status: 500 })
+
+  }
+}
