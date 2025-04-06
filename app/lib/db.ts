d2951d4e6b3cdd038d9484c2481b75fb906fdcf9
ksagar03 <sagar22krishna03@gmail.com>
2025-04-06 21:33:08 +0530

    Updates: Package Fix

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,25 +1,25 @@
-import sqlite3 from "sqlite3";
-import {open, Database} from "sqlite"
+// import sqlite3 from "sqlite3";
+// import {open, Database} from "sqlite"
 
-let db: Database | null = null;
+// let db: Database | null = null;
 
-const openDb = async (): Promise<Database> => {
+// const openDb = async (): Promise<Database> => {
 
-    if(!db){
-        db = await open({
-            filename: "./ExpCategories.sqlite",
-            driver: sqlite3.Database,
-        })
+//     if(!db){
+//         db = await open({
+//             filename: "./ExpCategories.sqlite",
+//             driver: sqlite3.Database,
+//         })
 
-        await db.exec(` CREATE TABLE IF NOT EXISTS categories (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT UNIQUE NOT NULL
-            )`)
-    }
-    return db
+//         await db.exec(` CREATE TABLE IF NOT EXISTS categories (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             name TEXT UNIQUE NOT NULL
+//             )`)
+//     }
+//     return db
 
 
-}
-export default openDb
+// }
+// export default openDb
 
 // This db.ts can not be used as the data stored in the sqlite will stored in the local storage or memory which can't be used to store the data in the cloud
